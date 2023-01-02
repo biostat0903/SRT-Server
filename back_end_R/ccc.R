2aa48271578c8faba1450a0fb353c1e9a0872f21
Sheng Yang <yangsheng@njmu.edu.cn>
2023-01-02 13:10:25 +0800

    Update ccc.R

--- back_end_R/ccc.R
+++ back_end_R/ccc.R
@@ -1,30 +1,27 @@
 #! /usr/bin/env Rscript
 # Identify cell-cell communication considering the spatial location
-# up-stream procedure: dr_cl.R, dr_cl_wr.R, dr_cl_sp.R
+# up-stream procedure: sdd.R, ct.R
 # down-stream procedure: ccc_plt.R
 
 # Set method_path
 method_path <- "/net/mulan/disk2/yasheng/stwebProject/01_code/01_method"
 
 # Load packages 
-library(hdf5r)
 library(SpaTalk)
 library(liana)
 library(bigreadr)
-library(dplyr)
-library(tibble)
-library(Matrix)
 library(SingleCellExperiment)
-library(scuttle)
-library(CellChat)
-library(data.table)
 library(Giotto)
 
+# Set parameters
 N_CORES = 4
+CELL_RES_PLATFORM <- c("Merfish", "Seqfish", "Generic_Cell")
+SPOT_RES_PLATFORM <- c("Visium", "Slideseq", "Generic_Spot")
 
 # Function 1: ccc.check
 ccc.check <- function(data_path1 = NULL,                 ## String: output path of ct or sdd procedure
                       data_path2 = NULL,                 ## String: output path of qc procedure
+                      data_path3 = NULL,                 ## String: output path of decon procedure
                       submodule = NULL,                  ## String: cell typing sub-modules: {CT_PCA}, {SDD_sPCA}, {CL_jo}
                       methods = NULL,                    ## String: cell typing methods: {Seurat}, {SpatialPCA}, {BASS}
                       Seurat_resolutions = NULL,         ## String: resolution
@@ -39,88 +36,110 @@ ccc.check <- function(data_path1 = NULL,                 ## String: output path 
                       out_path = "./"                    ## String: output path of ccc procedure              
 ){
   
-  ## check file
-  ## load inputs
+  ## Load general parameters
+  check_file <- paste0(data_path2, "/qc_check_file.txt")
   call_file <- paste0(data_path2, "/qc_call_file.txt")
-  if(!file.exists(call_file)){
+  if(!file.exists(call_file) | !file.exists(check_file)){
     
     stop("No QC file! Please run QC module!")
-  } else {
+  } else{
     
-    ### load count matrix
     qc_param <- read.table(call_file)[, 1]
     spatial_data_filename <- qc_param[1]
     sample_size <- qc_param[2]
+    qc_param <- read.table(check_file)[, 1]
+    platform <- qc_param[2]
   }
+  ## Set parameters for different platforms
+  if(platform %in% SPOT_RES_PLATFORM){
+    
+    post_file <- read.table(paste0(data_path1, "/sdd_post_file.txt"))[,1]
+    ### Set clustering joint module parameters
+    if(grepl("jo", submodule)){
+      
+      if(methods == "BASS"){
+        
+        grid_use <- paste0("R", BASS_domainNum, "_C", BASS_cellNum)
+      }
+      check_file <- c(spatial_data_filename, submodule, methods,
+                      sample_size, grid_use, 
+                      ccc_method, ccc_species, ccc_db, NA,
+                      post_file[2])
+    }
+    ### Set spatial domain detection modules
+    if(grepl("SDD", submodule)){
+      
+      if(methods == "SpatialPCA"){
+        
+        call_file <- read.table(paste0(data_path1, "/sdd_call_file.txt"))[,1]
+        clut_num <- as.numeric(call_file[2])
+        grid_use <- paste0("clust_", min(as.numeric(SpatialPCA_domainNum), clut_num))
+      }
+      
+      check_file <- c(spatial_data_filename, submodule, methods,
+                      sample_size, grid_use, 
+                      ccc_method, ccc_species, ccc_db, NA,
+                      post_file[2])
+    } 
+  }
+  if(platform %in% CELL_RES_PLATFORM){
+    
+    post_file <- read.table(paste0(data_path1, "/ct_post_file.txt"))[, 1]
+    ### Set clustering joint module parameters
+    if(grepl("jo", submodule)){
+      
+      if(methods == "BASS"){
+        
+        grid_use <- paste0("R", BASS_domainNum, "_C", BASS_cellNum)
+      }
+      check_file <- c(spatial_data_filename, submodule, methods,
+                      sample_size, grid_use, 
+                      ccc_method, ccc_species, ccc_db, NA,
+                      post_file[2], post_file[1])
+    }
+    ### Set spatial domain detection modules
+    if(grepl("CT", submodule)){
+      
+      if (methods == "Seurat"){
+        
+        grid_pc <- ifelse(sample_size == 1,
+                          paste0("cluster"),
+                          paste0("cluster_anchor", Seurat_anchors,
+                                 "_dim", Seurat_dims))
+        grid_use <- paste0(grid_pc, "_res", Seurat_resolutions)
+      }
+      check_file <- c(spatial_data_filename, submodule, methods,
+                      sample_size, grid_use, 
+                      ccc_method, ccc_species, ccc_db, NA,
+                      post_file[1])
+    }
+  }
+  ## Set parameters for spatalk
+  if (ccc_method == "spatalk"){
   
-  #
-  ## Set cell typing module parameters
-  if(grepl("CT", submodule)) {
+    if(is.null(data_path3)) {
     
-    if (methods == "Seurat"){
-      
-      grid_pc <- ifelse(sample_size == 1,
-                        paste0("cluster"),
-                        paste0("cluster_anchor", Seurat_anchors,
-                               "_dim", Seurat_dims))
-      grid_use <- paste0(grid_pc, "_res", Seurat_resolutions)
-    }
-    post_file <- read.table(paste0(data_path1, "/ct_post_file.txt"))[,1]
-    check_file <- c(spatial_data_filename, submodule, methods,
-                    sample_size, grid_use, 
-                    ccc_method, ccc_species, ccc_db, 
-                    post_file[1])
-  }
-  ## Set clustering joint module parameters
-  if(grepl("jo", submodule)){
-    
-    if(methods == "BASS"){
-      
-      grid_use <- paste0("R", BASS_domainNum, "_C", BASS_cellNum)
-    }
-    if(file.exists(paste0(data_path1, "/ct_post_file.txt"))){
-      
-      post_file <- read.table(paste0(data_path1, "/ct_post_file.txt"))[,1]
+      stop("spatalk needs the result of DECON!")  
     } else {
       
-      post_file <- read.table(paste0(data_path1, "/sdd_post_file.txt"))[,1]
+      check_file[is.na(check_file)] <- data_path3
     }
-    check_file <- c(spatial_data_filename, submodule, methods,
-                    sample_size, grid_use, 
-                    ccc_method, ccc_species, ccc_db, 
-                    post_file[1], post_file[2])
   }
-  
-  ## Set spatial domain detection modules
-  if(grepl("SDD", submodule)){
-    
-    if(methods == "SpatialPCA"){
-      
-      call_file <- read.table(paste0(data_path1, "/sdd_call_file.txt"))[,1]
-	  cat(call_file[1], "\n")
-      clut_num <- as.numeric(call_file[2])
-      grid_use <- paste0("clust_", min(as.numeric(SpatialPCA_domainNum), clut_num))
-    }
-    post_file <- read.table(paste0(data_path1, "/sdd_post_file.txt"))[,1]
-    check_file <- c(spatial_data_filename, submodule, methods,
-                    sample_size, grid_use, 
-                    ccc_method, ccc_species, ccc_db,  
-                    post_file[2])
-  }  
-  
   ## output file
   write.table(check_file, 
               file = paste0(out_path, "/ccc_check_file.txt"), 
               row.names = F, quote = F, col.names = F)
   
   return(0)
-  
 } 
 
 # Function 2: ccc test
 ccc.func <- function(st_list = NULL,
                      cluster_df = NULL,
                      grid_use = NULL,
+                     decon_path = NULL,
+                     platform = NULL, 
+                     ccc_type = "ccc",
                      ccc_method,
                      ccc_species,
                      ccc_db){
@@ -145,26 +164,17 @@ ccc.func <- function(st_list = NULL,
     }
   }
   message(paste0("We use ", ccc_db, " to fit ", ccc_method, "!"))
-  
-  
   ## format expression data and annotation data
-  # expression data
   count_merged <- purrr::reduce(st_list[["count_list"]], function(x, y) {
     cbind(x = x, y = y)
   })
   coord_merged <- as.data.frame(Reduce(rbind, st_list[["coord_list"]]))
-  
-  # make names
   colnames(count_merged) <- rownames(coord_merged) <- 
     make.names(colnames(count_merged))
-  
-  # annotation data
   annot_dat <- data.frame(cell = rownames(coord_merged),
                           x = coord_merged[,1],
                           y = coord_merged[,2])
-  annot_dat$celltype <- cluster_df[[grid_use]]#[match(annot_dat$cell, 
-                                                     # make.names(cluster_df$cell))]
-  
+  annot_dat$celltype <- cluster_df[[grid_use]]
   ## fit SpaTalk model
   if (ccc_method == "spatalk") {
     colnames(LR_data) <- c("ligand", "receptor")
@@ -172,12 +182,13 @@ ccc.func <- function(st_list = NULL,
     ccc_mat <- SpaTalk.test(expr_dat = count_merged,
                             annot_dat = annot_dat,
                             ccc_species = ccc_species,
+                            decon_path = decon_path,
+                            platform = platform, 
                             p_thresh = 1,
                             ccc_method = ccc_method,
                             ccc_db = LR_data,
                             ccc_pathway = NULL)
   }
-  
   ## output ccc_mat_list and write call_file
   return(ccc_mat)
 }
@@ -187,6 +198,8 @@ SpaTalk.test <- function(expr_dat = NULL,
                          annot_dat = NULL,
                          p_thresh = 1,
                          ccc_species = NULL,
+                         decon_path = NULL,
+                         platform = NULL, 
                          ccc_method = NULL,
                          ccc_db = NULL,
                          ccc_pathway = NULL,
@@ -198,13 +211,45 @@ SpaTalk.test <- function(expr_dat = NULL,
     
     ccc_pathway <- SpaTalk::pathways
   }
-  obj <- createSpaTalk(st_data = as.matrix(expr_dat),
-                       st_meta = annot_dat[, -4],
-                       species = ccc_species,
-                       if_st_is_sc = T,
-                       spot_max_cell = 1,
-                       celltype = annot_dat$celltype %>% as.character())
-  
+  if (platform %in% c("Visium", "Slideseq")){
+    
+    ## build SpaTalk object
+    load(paste0(decon_path, "/decon_result_list.RData"))
+    decon_prop <- plyr::alply(1: length(decon_result_list), 1, function(pp)
+      decon_result_list[[pp]]$Proportion_CARD) %>%
+      Reduce(rbind, .)
+    spot_label <- gsub("-", ".", rownames(decon_prop))
+    rownames(decon_prop) <- spot_label
+    colnames(annot_dat)[1] <- "spot"
+    expr_dat_sub <- expr_dat[, colnames(expr_dat) %in% spot_label]
+    annot_dat_sub <- annot_dat[annot_dat[, 1] %in% spot_label, ]
+    obj <- createSpaTalk(st_data = as.matrix(expr_dat_sub),
+                         st_meta = annot_dat_sub[, c(1, 2, 3)],
+                         species = ccc_species,
+                         if_st_is_sc = F,
+                         spot_max_cell = 6)
+    ## perform deconvolution by DECON result
+    ref_dat <- read.table(paste0(decon_path, "/decon_check_file.txt"))[3, 1]
+    load(paste0(ref_dat, "/sc_count.RData"))
+    load(paste0(ref_dat,"/sc_meta.RData"))
+    celltype <- gsub(" ", "_", sc_meta$cellType)
+    obj <- dec_celltype(obj, 
+                        sc_data = as.matrix(sc_count),
+                        sc_celltype = celltype, 
+                        if_doParallel = T, 
+                        use_n_cores = N_CORES, 
+                        dec_result = decon_prop)
+
+  }
+  if (platform %in% c("Merfish", "Seqfish", "Generic")){
+    
+    obj <- createSpaTalk(st_data = as.matrix(expr_dat),
+                         st_meta = annot_dat[, -4],
+                         species = ccc_species,
+                         if_st_is_sc = T,
+                         spot_max_cell = 1,
+                         celltype = annot_dat$celltype %>% as.character())
+  }
   ## Filter LRIs with downstream targets
   obj <- tryCatch({
     
@@ -212,7 +257,7 @@ SpaTalk.test <- function(expr_dat = NULL,
                  lrpairs = ccc_db, 
                  pathways = ccc_pathway,
                  if_doParallel = T,
-                 use_n_cores = n_cores)
+                 use_n_cores = N_CORES)
   }, error = function(e){
     
     print("ERROR")
@@ -233,7 +278,7 @@ SpaTalk.test <- function(expr_dat = NULL,
                        pvalue = p_thresh,
                        co_exp_ratio = 0.1,
                        if_doParallel = T,
-                       use_n_cores = n_cores)
+                       use_n_cores = N_CORES)
     ccc_mat <- obj@lrpair
     colnames(ccc_mat)[4:5] <- c("source", "target")
     return(ccc_mat)
@@ -247,7 +292,7 @@ ccc.call <- function(out_path            ## String: output path of ccc procedure
   ## Load io code
   source(paste0(method_path, "/io.R"))
   
-  ## load inputs
+  ## Load inputs
   check_file <- read.table(paste0(out_path, "/ccc_check_file.txt"))[, 1]
   spatial_data_filename <- check_file[1]
   submodule <- check_file[2]
@@ -257,57 +302,57 @@ ccc.call <- function(out_path            ## String: output path of ccc procedure
   ccc_method <- check_file[6]
   ccc_species <- check_file[7]
   ccc_db <- check_file[8]
-    
-  ## load st data
+  decon_path <- check_file[9]
+  
+  ## Load st data
   st_list <- h5data.load(spatial_data_filename,         
                          sample_size = sample_size,       
                          load_count = TRUE,     
                          normalization = FALSE,  
                          load_coord = TRUE,    
                          coordinate = TRUE)
-    
+  platform <- st_list$platform
   ## define ccc in different modules
   result_file <- paste0(out_path, "/ccc_call_result.RData")
   clus_file <- paste0(out_path, "/ccc_call_cluster.RData")
-  if(grepl("CT", submodule)) {
-    
-    ct_df <- fread2(check_file[9])
-    ct_ccc_mat <- ccc.func(st_list = st_list,
-                       cluster_df = ct_df,
-                       grid_use = grid_use,
-                       ccc_method = ccc_method,
-                       ccc_species = ccc_species,
-                       ccc_db = ccc_db)
-    
-    save(ct_ccc_mat, file = result_file)
-    save(ct_df, file = clus_file)
-  }
-  if(grepl("jo", submodule)) {
-    #
-    ct_df <- fread2(check_file[9])
+  if(!grepl("SDD", submodule)) {
+
+    sdd_df <- sdd_ccc_mat <- NULL
+    if(grepl("jo", submodule) & length(check_file) > 10){
+
+      sdd_df <- fread2(check_file[length(check_file)])
+      sdd_ccc_mat <- ccc.func(st_list = st_list,
+                              cluster_df = sdd_df,
+                              grid_use = grid_use,      
+                              decon_path = decon_path,
+                              platform = st_list$platform, 
+                              ccc_type = "ddc",
+                              ccc_method = ccc_method,
+                              ccc_species = ccc_species,
+                              ccc_db = ccc_db)
+    }
+    ct_df <- fread2(check_file[10])
     ct_ccc_mat <- ccc.func(st_list = st_list,
                            cluster_df = ct_df,
                            grid_use = grid_use,
+                           decon_path = decon_path,
+                           platform = st_list$platform,
+                           ccc_type = "ccc",
                            ccc_method = ccc_method,
                            ccc_species = ccc_species,
                            ccc_db = ccc_db)
-    #
+    save(ct_ccc_mat, sdd_ccc_mat, file = result_file)
+    save(ct_df, sdd_df, file = clus_file)
+  } 
+  if(grepl("SDD", submodule)){
+
     sdd_df <- fread2(check_file[10])
     sdd_ccc_mat <- ccc.func(st_list = st_list,
                             cluster_df = sdd_df,
-                            grid_use = grid_use,
-                            ccc_method = ccc_method,
-                            ccc_species = ccc_species,
-                            ccc_db = ccc_db)
-    save(ct_ccc_mat, sdd_ccc_mat, file = result_file)
-    save(ct_df,sdd_df, file = clus_file)
-  }
-  if(grepl("SDD", submodule)) {
-    
-    sdd_df <- fread2(check_file[9])
-    sdd_ccc_mat <- ccc.func(st_list = st_list,
-                            cluster_df = sdd_df,
-                            grid_use = grid_use,
+                            grid_use = grid_use,      
+                            decon_path = decon_path,
+                            platform = st_list$platform, 
+                            ccc_type = "ddc",
                             ccc_method = ccc_method,
                             ccc_species = ccc_species,
                             ccc_db = ccc_db)
@@ -343,7 +388,7 @@ ccc.post <- function(out_path            ## String: output path of ccc procedure
     system(paste0("mkdir -p ", result_dir))
   }
   
-  if(grepl("CT", submodule) | grepl("jo", submodule)) {
+  if(grepl("CT", submodule)) {
     
     ct_ccc_mat_file <- paste0(result_dir, 
                               "/ct_ccc_", ccc_method,  "_", ccc_db, "_mat.txt")
@@ -366,21 +411,3 @@ ccc.post <- function(out_path            ## String: output path of ccc procedure
   return(0)
   
 }
-
-
-# ###################
-# ### test code
-# data_path1 <- "/pt_data/494433291@qq.com/0a4d7f5a254e45028b4d74a998958526/tools-output/wf-483227122920325696/job-SDD-483233367291068992"
-# data_path2 <- "/pt_data/494433291@qq.com/0a4d7f5a254e45028b4d74a998958526/tools-output/wf-483227122920325696/job-QC-483227148551717440"
-# output_path <- "/pt_data/494433291@qq.com/0a4d7f5a254e45028b4d74a998958526/tools-output/wf-483227122920325696/job-CCC-483276849707745856"
-# ccc.check(data_path1 = data_path1,
-          # data_path2 = data_path2,
-          # submodule = "SDD_sPCA",
-          # methods = "SpatialPCA",
-          # SpatialPCA_domainNum = "10",
-          # ccc_method = "spatalk",
-          # ccc_species = "Mouse",
-          # ccc_db = "SpaTalk.DB",
-          # out_path = output_path)
-# ccc.call(out_path = output_path)
-# ccc.post(out_path = output_path)
